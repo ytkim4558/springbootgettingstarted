25cfc9a0eb8d90f7d00f0e6aa62008ac910968db
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-18 21:14:21 +0900

    Undertow 실습

--- src/main/java/me/facemoktak/Application.java
+++ src/main/java/me/facemoktak/Application.java
@@ -1,8 +1,8 @@
 package me.facemoktak;
-
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
+//
+//import org.apache.catalina.Context;
+//import org.apache.catalina.LifecycleException;
+//import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,36 +19,36 @@ import java.nio.file.Files;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws LifecycleException, IOException {
-//        SpringApplication application = new SpringApplication(Application.class);
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
 //        application.setWebApplicationType(WebApplicationType.NONE);
-//        application.run(args);
-        String docBase = Files.createTempDirectory("tomcat-basedir").toString();
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-
-        Context context = tomcat.addContext("/", docBase);
-
-        HttpServlet servlet = new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                PrintWriter writer = resp.getWriter();
-                writer.println("<html><head><title>");
-                writer.println("Hey, Tomcat");
-                writer.println("</title></head>");
-                writer.println("<body><h1>Hello Tomcat</h1></body>");
-                writer.println("</html>");
-            }
-        };
-
-        String servletName = "helloServlet";
-        tomcat.addServlet("/", servletName, servlet);
-        context.addServletMappingDecoded("/hello", servletName);
-
-
-        tomcat.getConnector();
-        tomcat.start();
-        tomcat.getServer().await();
+        application.run(args);
+//        String docBase = Files.createTempDirectory("tomcat-basedir").toString();
+//        Tomcat tomcat = new Tomcat();
+//        tomcat.setPort(8080);
+//
+//        Context context = tomcat.addContext("/", docBase);
+//
+//        HttpServlet servlet = new HttpServlet() {
+//            @Override
+//            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                PrintWriter writer = resp.getWriter();
+//                writer.println("<html><head><title>");
+//                writer.println("Hey, Tomcat");
+//                writer.println("</title></head>");
+//                writer.println("<body><h1>Hello Tomcat</h1></body>");
+//                writer.println("</html>");
+//            }
+//        };
+//
+//        String servletName = "helloServlet";
+//        tomcat.addServlet("/", servletName, servlet);
+//        context.addServletMappingDecoded("/hello", servletName);
+//
+//
+//        tomcat.getConnector();
+//        tomcat.start();
+//        tomcat.getServer().await();
 
     }
 }
