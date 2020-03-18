1b8d4fb8c7f61db6debc9ee748ccef2bb6729124
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-18 20:35:40 +0900

    Bean 생성

--- src/main/java/me/facemoktak/Application.java
+++ src/main/java/me/facemoktak/Application.java
@@ -3,7 +3,6 @@ package me.facemoktak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -12,13 +11,5 @@ public class Application {
         SpringApplication application = new SpringApplication(Application.class);
         application.setWebApplicationType(WebApplicationType.NONE);
         application.run(args);
-    }
-
-    @Bean
-    public Holoman holoman() {
-        Holoman holoman = new Holoman();
-        holoman.setName("yongtak");
-        holoman.setHowLong(36);
-        return holoman;
     }
 }
